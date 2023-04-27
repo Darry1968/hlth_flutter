9fb4c0054d29d83efc2ca5fc02791c56fe2510c7
Darshan Soni <sonidarshan200@gmail.com>
2023-04-27 12:05:55 +0530

    first commit

---
+++ lib/calorie_counter.dart
@@ -0,0 +1,106 @@
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
+
+class FoodItem {
+  String name;
+  int calories;
+
+  FoodItem(this.name, this.calories);
+}
+
+class CalorieCounterWidget extends StatefulWidget {
+  const CalorieCounterWidget({Key? key}) : super(key: key);
+
+  @override
+  _CalorieCounterWidgetState createState() => _CalorieCounterWidgetState();
+}
+
+class _CalorieCounterWidgetState extends State<CalorieCounterWidget> {
+  List<FoodItem> _foodItems = [];
+
+  void _addFoodItem(String name, int calories) {
+    setState(() {
+      _foodItems.add(FoodItem(name, calories));
+    });
+  }
+
+  void _deleteFoodItem(int index) {
+    setState(() {
+      _foodItems.removeAt(index);
+    });
+  }
+
+  int _calculateTotalCalories() {
+    int totalCalories = 0;
+    for (FoodItem foodItem in _foodItems) {
+      totalCalories += foodItem.calories;
+    }
+
+    return totalCalories;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calorie Counter'),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Food Item',
+            ),
+            onSubmitted: (String value) {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: Text('Add Calories'),
+                    content: TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Calories',
+                      ),
+                      keyboardType: TextInputType.number,
+                      onSubmitted: (String value) {
+                        _addFoodItem(value, int.parse(value));
+                        Navigator.pop(context);
+                      },
+                    ),
+                  );
+                },
+              );
+            },
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _foodItems.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: Text(_foodItems[index].name),
+                  subtitle: Text('${_foodItems[index].calories} calories'),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () {
+                      _deleteFoodItem(index);
+                    },
+                  ),
+                );
+              },
+            ),
+          ),
+          Text(
+            'Total Calories: ${_calculateTotalCalories()}',
+            style: TextStyle(fontSize: 18.0),
+          ),
+        ],
+      ),
+    );
+  }
+}
